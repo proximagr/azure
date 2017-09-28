8d6bc48a9f8af168dfd60c00013a80c632ff739d
proximagr <proximagr@hotmail.com>
2017-09-28 10:53:08 +0300

    Add files via upload

---
+++ azurefiles.ps1
@@ -0,0 +1,5 @@
+﻿$ContosoStoreKey = (Get-AzureStorageKey -StorageAccountName ppdrazfiles).Primary
+$storageContext=New-AzureStorageContext -StorageAccountName ppdrazfiles -StorageAccountKey $ContosoStoreKey
+$storageContext |  New-AzureStorageShare -Name websites
+$storageContext |  New-AzureStorageShare -Name m2m
+$storageContext |  New-AzureStorageShare -Name certificates
