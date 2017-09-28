847509ed0b346cd41cdce6c2611cc9e82c2c5029
proximagr <proximagr@hotmail.com>
2017-09-28 16:03:34 +0300

    Add files via upload

---
+++ azurefiles.ps1
@@ -0,0 +1,13 @@
+﻿Param (
+  [Parameter()]
+  [String]$SAKey,
+  [String]$SAName
+)
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module Azure -Confirm:$False
+Import-Module Azure
+$storageContext = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $SAKey
+$storageContext |  New-AzureStorageShare -Name websites
+$storageContext |  New-AzureStorageShare -Name m2m
+$storageContext |  New-AzureStorageShare -Name certificates
